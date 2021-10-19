3e207c5ca3954b580c0c694fdd9a5a54dbd2ae87
Theo Kaleel <theo.kaleel@gmail.com>
2021-10-19 16:34:05 -0700

    track multiple likes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function addLike(){
-    var element = document.querySelector('#likes');
+function addLike(id){
+    var element = document.querySelector(id);
     var count= parseInt(element.innerHTML);
     element.innerHTML=count+1;
 }
